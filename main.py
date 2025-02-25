aef0a2757c596a9ea428a6377ebfbda2bced2231
Julio <ju.crr2@gmail.com>
2025-02-25 19:04:44 -0300

    Numpy até os exercicios

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ import numpy as np
 # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 # Operações com Numpy arrays
-arr1 = np.array([1,2,3,4])
-arr2 = np.array([5,6,7,8])
+#arr1 = np.array([1,2,3,4])
+#arr2 = np.array([5,6,7,8])
 
 #print(arr1.min(), arr1.max())
 #print(arr1.argmin(), arr1.argmax())
@@ -41,6 +41,50 @@ arr2 = np.array([5,6,7,8])
 #print(arr1.mean(), arr1.sum())
 
 # Operações entre dois ou mais arrays
+# Somando os valores com array
+#arr3 = arr1 + arr2
+#print(arr3)
 
-arr3 = arr1 + arr2
-print(arr3)
+# Concatenando as arrays
+#arr4 = np.concatenate([arr1,arr2])
+#print(arr4)
+
+# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+# Reshape de numpy arrays
+
+#mtz = np.arange(9).reshape(3,3)
+#print(mtz)
+
+#print(mtz.size)
+#print(mtz.shape)
+#print(mtz.ndim)
+
+#  Operações nas linhas e colunas da matriz
+
+#print(mtz.sum(axis=0)) # Somando colunas
+#print(mtz.sum(axis=1)) # Somando linhas
+
+#print(mtz.sum(axis=0)[0]) # Somando a primeira coluna apenas
+#print(mtz.sum(axis=1)[0]) # Somando a primeira linha apenas
+
+# Broadcasting
+
+#print(mtz/2)
+
+# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+# Elementos aleatorios e unicos
+
+# Gerando numeros aleatorios
+np.random.seed(10) # Ao adicionar uma semente, voce gera os mesmos numeros aleatorios
+arr = np.random.randint(20,50,10) # Uni dimensional
+print(arr)
+
+arr2 = np.random.randint(20,50,[2,2]) #  Bi dimensional
+print(arr2)
+
+# Extraindo elementos unicos com unique
+arr3 = np.random.randint(20,50,[5,5])
+print(np.unique(arr3, return_counts=True))
+
